e036846503b9f5be0a33929c3ea198fa91d3eee3
AlessiaCaccamo <118570185+AlessiaCaccamo@users.noreply.github.com>
2025-02-13 18:02:50 +0000

    Update generate_spectrum.m

--- generate_spectrum.m
+++ generate_spectrum.m
@@ -1,3 +1,4 @@
+% Alessia Caccamo, University of Exeter, September 2023
 function psd_m = generate_spectrum(param_values, freq) % Return the model spectrum
 x0 = zeros(1,13); % Initial conditions
 x=zeros(1,13); % Vector of state variables
