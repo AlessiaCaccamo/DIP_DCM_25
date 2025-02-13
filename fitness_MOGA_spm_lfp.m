7025b5c0ef2a88fb17c7870fdf269fbc45820619
AlessiaCaccamo <118570185+AlessiaCaccamo@users.noreply.github.com>
2025-02-13 18:02:23 +0000

    fitness_MOGA_spm_lfp.m

--- fitness_MOGA_spm_lfp.m
+++ fitness_MOGA_spm_lfp.m
@@ -1,3 +1,4 @@
+% Alessia Caccamo, University of Exeter, September 2023
 function fitall = fitness_MOGA_spm_lfp(param_values, paramsvec, data, paramstoest,freq)
 warning('off', 'signal:finddelay:noSignificantCorrelationScalar');
 MOGA_lfp_params;
