efc3e60c3e61194bcca29a3df6239ad9ce9d9424
AlessiaCaccamo <118570185+AlessiaCaccamo@users.noreply.github.com>
2025-02-14 12:19:11 +0000

    Update run_lfp_hybrid.m

--- run_lfp_hybrid.m
+++ run_lfp_hybrid.m
@@ -1,4 +1,4 @@
-% Alessia Caccamo, University of Exeter, September 2023
+% PhD Mathematics and Statistics, Thesis Chapter 1, Alessia Caccamo, University of Exeter, January 2024
 function DCM=run_lfp_hybrid(MOGA_params,nsim,data,freq) %Return DCM structure
 
 DCM = [];
