d6578bd2ce99318292e1413ba3ae0a293513eef5
AlessiaCaccamo <118570185+AlessiaCaccamo@users.noreply.github.com>
2025-09-24 13:45:09 +0100

    Update load_data.m

--- load_data.m
+++ load_data.m
@@ -1,5 +1,5 @@
 function [data,freq] =load_data(i)   % Load the dataset (EEG spectrum) to be modelled 
-    load('Spectra_all_subj_norm_final.mat'); % 'average_spectrum', 'average_spectrum_patient', 'f', 'f_patient', 'mas_control', 'mas_patient');
+    load('your_data_file.mat'); 
     f=your_frequency_vector;
     if i == 1 % Load the power spectrum associated with condition 1
     data = log(your_psd_vector_condition1)'; % considering you want to compare spectra of two experimental conditions
